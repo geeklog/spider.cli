389071c64566beaeaf9ccefbf8acd0246c3dcf67
Mike Wu <mike@bitguild.com>
2020-05-08 13:39:16 +0800

    clean up

--- spider.js
+++ spider.js
@@ -10,7 +10,6 @@ const entities = new (require('html-entities').XmlEntities)();
 const JQ = require('node-jq');
 const {collectStream, monitorStream, isStream} = require('./stream');
 const {isURL, resolveURLs, uniqOutput, concurrent, concurrently} = require('./helper');
-const {multiProgressing} = require('./cli');
 
 const userAgents = {
   chrome: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36',
